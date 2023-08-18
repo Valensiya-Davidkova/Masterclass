fba7555dd05c49b6669f5c67baf9efab3d522431
Valensiya-Davidkova <124916900+Valensiya-Davidkova@users.noreply.github.com>
2023-08-18 11:32:27 +0300

    Delete hello-todo directory

--- hello-todo/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t org.example/hello-todo .
-docker rm -f hello-todo || true && docker run -d -p 8080:8080 -p 4848:4848 --name hello-todo org.example/hello-todo 

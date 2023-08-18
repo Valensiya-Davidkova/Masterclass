8a5fba7fc0b13ec383400555a69bdc75076854df
Valensiya-Davidkova <124916900+Valensiya-Davidkova@users.noreply.github.com>
2023-08-18 11:33:37 +0300

    Add files via upload

---
+++ hello-todo/src/main/java/academ/learnprogramming/rest/TodoRest.java
@@ -0,0 +1,54 @@
+package academ.learnprogramming.rest;
+
+import academy.learnprogramming.service.ToDoService;
+import com.academy.learnprogramming.entity.Todo;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("todo")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class TodoRest {
+    @Inject
+    ToDoService toDoService;
+    @Path("new")
+    @POST
+    public Response createTodo(Todo todo){
+        toDoService.createToDo(todo);
+        return Response.ok(todo).build();
+    }
+
+    @Path("update")
+    @PUT
+    public Response updateTodo(Todo todo){
+        toDoService.updateToDo(todo);
+        return Response.ok(todo).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Todo getTodo(@PathParam("id") Long id){
+        return toDoService.findToDoById(id);
+    }
+
+
+    @Path("list")
+    @GET
+    public List<Todo> getTodos(){
+        return toDoService.getTodos();
+    }
+
+    @Path("status")
+    @POST
+    public Response markAsComplete(@QueryParam("id") Long id){
+        Todo todo=toDoService.findToDoById(id);
+        todo.setIsCompleted(true);
+        toDoService.updateToDo(todo);
+        return Response.ok(todo).build();
+    }
+
+}
